8ba0797464d83b10ad39ed35f73e66218fca630a
oneisx <1329166894@qq.com>
2021-12-22 00:17:35 +0800

    yaml

--- util/file.go
+++ util/file.go
@@ -22,7 +22,7 @@ func PutYaml(key string, value interface{}) {
 
 func readYaml() map[string]interface{} {
     m := make(map[string]interface{})
-    data := read("D:\\igo\\test\\.config.yaml")
+    data := read(configFilePath())
     err := yaml.Unmarshal(data, &m)
     if err != nil {
         log.Fatalf("error: %v", err)
@@ -35,10 +35,10 @@ func writeYaml(in map[string]interface{}) {
     if err != nil {
         log.Fatalf("error: %v", err)
     }
-    write("D:\\igo\\test\\.config.yaml", d)
+    write(configFilePath(), d)
 }
 
-func read(filename string) []byte{
+func read(filename string) []byte {
     var (
         err     error
         content []byte
@@ -46,15 +46,13 @@ func read(filename string) []byte{
     content, err = ioutil.ReadFile(filename)
     if err != nil {
         fmt.Println(err)
+        _ = os.MkdirAll(igoHomeDir(), os.ModePerm)
         return nil
     }
     return content
 }
 
 func write(filename string, data []byte) {
-    var (
-        err error
-    )
     // 拿到一个文件对象
     fileObj, err := os.Create(filename)
     if err != nil {
@@ -65,4 +63,17 @@ func write(filename string, data []byte) {
     writer := bufio.NewWriter(fileObj)
     defer writer.Flush()
     _, _ = writer.Write(data)
+}
+
+func configFilePath() string {
+    return igoHomeDir() + string(os.PathSeparator) + ".config.yaml"
+}
+
+func userHomeDir() string{
+    dir, _ := os.UserHomeDir()
+    return dir
+}
+
+func igoHomeDir() string {
+    return userHomeDir() + string(os.PathSeparator) + ".igo"
 }
